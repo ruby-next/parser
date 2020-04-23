0209aab11fdae3bfa3820468dbb46691be7ba789
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-04-23 11:34:07 +0300

    + rubynext.y: add method reference operator
    From https://github.com/whitequark/parser/pull/556

--- lib/parser/ruby-next/builder.rb
+++ lib/parser/ruby-next/builder.rb
@@ -6,5 +6,9 @@ require_relative "meta"
 module Parser
   # Add RubyNext specific builder methods
   module Builders::Next
+    def method_ref(receiver, dot_t, selector_t)
+      n(:meth_ref, [ receiver, value(selector_t).to_sym ],
+          send_map(receiver, dot_t, selector_t, nil, [], nil))
+    end
   end
 end
