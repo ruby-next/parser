606f4c373e86ba90fd0f29c2aaf88cc6392d850f
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-04-23 11:34:07 +0300

    + rubynext.y: add method reference operator
    From https://github.com/whitequark/parser/pull/556

---
+++ test/ruby-next/test_parser.rb
@@ -0,0 +1,70 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
+require 'helper'
+require 'parse_helper'
+
+Parser::Builders::Default.modernize
+
+class TestParser < Minitest::Test
+  include ParseHelper
+
+  def parser_for_ruby_version(version)
+    parser = super
+    parser.diagnostics.all_errors_are_fatal = true
+
+    %w(foo bar baz).each do |metasyntactic_var|
+      parser.static_env.declare(metasyntactic_var)
+    end
+
+    parser
+  end
+
+  SINCE_NEXT = %w(next)
+
+  def test_meth_ref__27
+    assert_parses(
+      s(:meth_ref, s(:lvar, :foo), :bar),
+      %q{foo.:bar},
+      %q{   ^^ dot
+        |     ~~~ selector
+        |~~~~~~~~ expression},
+      SINCE_NEXT)
+
+    assert_parses(
+      s(:meth_ref, s(:lvar, :foo), :+@),
+      %q{foo.:+@},
+      %q{   ^^ dot
+        |     ~~ selector
+        |~~~~~~~ expression},
+      SINCE_NEXT)
+  end
+
+  def test_meth_ref__before_27
+    assert_diagnoses(
+      [:error, :unexpected_token, { :token => 'tCOLON' }],
+      %q{foo.:bar},
+      %q{    ^ location },
+      ALL_VERSIONS - SINCE_NEXT)
+
+    assert_diagnoses(
+      [:error, :unexpected_token, { :token => 'tCOLON' }],
+      %q{foo.:+@},
+      %q{    ^ location },
+      ALL_VERSIONS - SINCE_NEXT)
+  end
+
+  def test_meth_ref_unsupported_newlines
+    assert_diagnoses(
+      [:error, :unexpected_token, { :token => 'tCOLON' }],
+      %Q{foo. :+},
+      %q{     ^ location},
+      SINCE_NEXT)
+
+    assert_diagnoses(
+      [:error, :unexpected_token, { :token => 'tCOLON' }],
+      %Q{foo.: +},
+      %q{    ^ location},
+      SINCE_NEXT)
+  end
+end
