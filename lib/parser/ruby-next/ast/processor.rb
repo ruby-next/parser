b621772361817b278e0d8a16e109db1ef200f714
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-07-02 12:53:25 +0300

    - rubynext.y: revert def_e/defs_e

--- lib/parser/ruby-next/ast/processor.rb
+++ lib/parser/ruby-next/ast/processor.rb
@@ -12,11 +12,6 @@ module Parser
         end
       end
 
-      unless method_defined?(:on_def_e)
-        alias on_def_e    on_def
-        alias on_defs_e   on_defs
-      end
-
       unless method_defined?(:on_rasgn)
         alias on_rasgn    process_regular_node
         alias on_mrasgn   process_regular_node
--- lib/parser/ruby-next/parser_ext.rb
+++ lib/parser/ruby-next/parser_ext.rb
@@ -2,7 +2,7 @@
 
 require_relative "lexer"
 require_relative "builder"
-require_relative "source/map/endless_definition" unless defined?(Parser::Source::Map::EndlessDefinition)
+require_relative "source/map/method_definition" unless defined?(Parser::Source::Map::MethodDefinition)
 require_relative "ast/processor"
 
 module Parser
