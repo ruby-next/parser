67917e9434c97fa3436652f2fe8f4e8c93c490ef
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-09-28 11:54:56 +0300

    - ruby-next/builder.rb: do not raise on using _N for local vars

--- test/test_parser.rb
+++ test/test_parser.rb
@@ -10131,13 +10131,13 @@ class TestParser < Minitest::Test
       [:error, :reserved_for_numparam, { :name => '_1' }],
       %q{proc {_1 = nil}},
       %q{      ^^ location},
-      SINCE_3_0)
+      SINCE_3_0 - ['next'])
 
     assert_diagnoses(
       [:error, :reserved_for_numparam, { :name => '_2' }],
       %q{_2 = 1},
       %q{^^ location},
-      SINCE_3_0)
+      SINCE_3_0 - ['next'])
 
     # Arguments:
 
@@ -10206,7 +10206,7 @@ class TestParser < Minitest::Test
         [:error, :reserved_for_numparam, { :name => '_3' }],
         code,
         location,
-        SINCE_3_0)
+        SINCE_3_0 - ['next'])
     end
 
     # Method definitions:
@@ -10237,7 +10237,7 @@ class TestParser < Minitest::Test
         [:error, :reserved_for_numparam, { :name => '_5' }],
         code,
         location,
-        SINCE_3_0)
+        SINCE_3_0 - ['next'])
     end
   end
 
