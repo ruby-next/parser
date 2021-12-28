622ab90bbfcd59513b346f53edee6fd524b2771a
Vladimir Dementyev <dementiev.vm@gmail.com>
2021-12-28 17:13:32 +0300

    + rubynext.y: support ivars/gvars as p_variables

--- lib/parser/ruby-next/builder.rb
+++ lib/parser/ruby-next/builder.rb
@@ -11,6 +11,13 @@ module Parser
           send_map(receiver, dot_t, selector_t, nil, [], nil))
     end
 
+    def match_var(var)
+      return super(var) unless var.is_a?(::Parser::AST::Node)
+
+      n(:match_var, [ var ],
+        var_send_map(var))
+    end
+
     def check_reserved_for_numparam(name, loc)
       # We don't want to raise SyntaxError, 'cause we want to use _x vars for older Rubies.
       # The exception should be raised by Ruby itself for versions supporting numbered parameters
